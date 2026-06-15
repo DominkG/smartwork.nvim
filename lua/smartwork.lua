7a7353c7d57fd68261b6446737288307f1d2e5b0
DominkG <45515835+DominkG@users.noreply.github.com>
2026-06-15 13:17:42 +0200

    Implement dnslookup and ipman functions
    Added dnslookup and ipman functions for DNS and IP management.

--- lua/smartwork.lua
+++ lua/smartwork.lua
@@ -14,4 +14,25 @@ M.ticket = function()
 	vim.cmd("startinsert")
 end
 
+function M.dnslookup(domain)
+	local domain = domain
+	local word = vim.fn.getreg('') -- get blank/yank register
+	local result = vim.system({ "dig", "+short", word .. "." .. domain }, { text = true }):wait()
+	local ip = vim.trim(result.stdout)
+	if ip == "" then
+		vim.notify("Keine Adresse gefunden fuer" .. word .. "." .. domain .. " gefunden.", vim.log.levels.WARN)
+		return
+	end
+	vim.cmd("normal! e")
+	vim.api.nvim_put({"(" .. ip .. ")" }, "c", true, true)
+end
+
+function M.ipman()
+	local word = vim.fn.getreg('') -- get blank/yank register
+	local result = vim.system({ "ipman", "-a", word}, { text = true }):wait()
+	local output = result.stdout or ""
+	local lines = vim.split(output, "\n", { trimempty = true })
+	vim.api.nvim_put(lines, "c", true, true)
+end
+
 return M
